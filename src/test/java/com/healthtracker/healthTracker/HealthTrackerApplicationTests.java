5ce51cddecb980ce91ce3f0ca8998f461a9f99ee
Merry Cimakasky <mkcimakasky@gmail.com>
2019-05-29 10:58:44 -0700

    setup

---
+++ src/test/java/com/healthtracker/healthTracker/HealthTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.healthtracker.healthTracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HealthTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
